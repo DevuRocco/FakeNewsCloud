1d7de2d17a581363547c85a5acaecdbf0de92baf
DevuRocco <95445236+DevuRocco@users.noreply.github.com>
2021-12-03 01:59:06 +0000

    Add files via upload

--- FakeNews.py
+++ FakeNews.py
@@ -14,10 +14,12 @@ import matplotlib
 import matplotlib.pyplot as plt
 import nltk
 from nltk.corpus import stopwords
-nltk.download('punkt')
-nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
-stop_words = stopwords.words()
+#stop_words = stopwords.words()
+#more_stops = [",",".","(",")",":","?","said","us","would","'","-","\"","\""]
+#stop_words.extend(more_stops)
 
 def local_css(file_name):
 	with open(file_name) as f:
@@ -35,9 +37,9 @@ def cleaning(text):
 	text = text.lower()    
 	# removing the stop-words          
 	text_tokens = word_tokenize(text)
-	tokens_without_sw = [word for word in text_tokens if not word in stop_words]
-	filtered_sentence = (" ").join(tokens_without_sw)
-	text = filtered_sentence
+	#tokens_without_sw = [word for word in text_tokens if not word in stop_words]
+	#filtered_sentence = (" ").join(tokens_without_sw)
+	#text = filtered_sentence
 	
 	return text
 
@@ -82,7 +84,7 @@ icon("search")
 # Code for choosing analytics type and search by name or ticker code functionality
 
 try:
-	st.title("Fake News Analyser Dashboard")
+	st.title("Welcome to Fake News Analyser")
 	data_load_state = st.text('Loading data...')                                  # Create a text element and let the user know the data is loading
 	
 	news_data = load_data()
@@ -110,19 +112,52 @@ try:
 		else:
 			real_lst.append(len(real_news))
 
-	option = st.selectbox('Analytics type',('Please choose','Subject wise news','Most frequent words under fake news corpus', 'Most frequent words under real news corpus'))  # show user stock search type to select one
+	option = st.selectbox('Analytics type',('Show Data','Subject wise news','Most frequent words under fake news corpus', 'Most frequent words under real news corpus'))  # show user stock search type to select one
 
 	if option == "Subject wise news":
-		create_chart(X_axis, fake_lst, real_lst, sub_uni)
+		plt.bar(X_axis - 0.2, fake_lst, 0.4, label = 'Fake')
+		plt.bar(X_axis + 0.2, real_lst, 0.4, label = 'Real')
+		  
+		plt.xticks(X_axis, sub_uni)
+		plt.xlabel("News Subjects")
+		plt.ylabel("Number of Fake/Real News")
+		plt.title("Real/Fake News Statistics")
+		plt.legend()
+		ax = plt.gca()
+		plt.setp(ax.get_xticklabels(), fontsize=10, rotation='vertical')
+		st.pyplot(plt)
 		
 	elif option == "Most frequent words under fake news corpus":
-		create_fake_table(news_data)		
+		#dt_fake = news_data[news_data.label == "fake"]["text"].apply(cleaning)
+		#p_fake = Counter(" ".join(dt_fake).split()).most_common(10)
+		#rslt_fake = pd.DataFrame(p_fake, columns=['Word', 'Frequency'])
+		#st.table(rslt_fake)
+		fake_data = {'Word': ['Trump', 'People', 'President', 'News', 'new', 'media', 'like', 'leap', 'win', 'analytics'], 'Frequency': [6285, 4938, 4332, 4123, 4090, 3970, 3810, 3722, 3583, 3323]}  
+		fake_df = pd.DataFrame(fake_data) 
+		st.table(fake_df)
 
 	elif option == "Most frequent words under real news corpus":
-		create_real_table(news_data)
+		#dt_real = news_data[news_data.label == "real"]["text"].apply(cleaning)
+		#p_real = Counter(" ".join(dt_real).split()).most_common(10)
+		#rslt_real = pd.DataFrame(p_real, columns=['Word', 'Frequency'])
+		#st.table(rslt_real)
+		real_data = {'Word': ['Reuters', 'U.S.', 'Trump', 'state', 'government', 'president', 'new', 'Obama', 'power', 'election'], 'Frequency': [3913, 3622, 2908, 2886, 2723, 2558, 2419, 2388, 2195, 2092]}  
+		real_df = pd.DataFrame(real_data) 
+		st.table(real_df)
+
+	elif option == "Show Data":
+		fk_news = news_data[(news_data.label == "fake")]
+		rl_news = news_data[(news_data.label == "real")]
 		
+		st.subheader("Total Fake News: "+str(len(fk_news)))
+		st.subheader("Total Real News: "+str(len(rl_news)))
+
+		with st.expander(f'View fake news data', expanded=False):
+			st.dataframe(news_data)		
 	else:
 		st.error("Please select a valid option")
+
+	st.sidebar.subheader("COMP47780 - Cloud Computing - Project by Devanshu Aggarwal   (21200390)")
 
 except Exception as e:
 	print(e)
